b48ca21aa68a1cae0d024ab8f7bb06c392df7495
Osama Attia <108284913+osallines@users.noreply.github.com>
2024-08-21 20:09:58 +0300

    writing js comment

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById("add");
+const btn = document.getElementById("add"); // this is the button variable
 
 function addTask() {
     const input = document.getElementById("task-input");
