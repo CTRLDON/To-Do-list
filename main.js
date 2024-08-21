1b59650b5edef8475c057616f15363b2fa079ae2
Osama Attia <osallines@gmail.com>
2024-08-21 20:24:39 +0300

    this is a test case that Idk

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const btn = document.getElementById("add"); // this is the button variable
 
 function addTask()
 // add task function, this function will let you input the tasks
+// this is a new note
 {
     const input = document.getElementById("task-input"); // this is input field constant
     console.log(input.value);
