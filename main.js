2274b346b23a9d359d8bbaf8419c64cee7eccb29
CTRLDON <a.m.elatawy@gmail.com>
2024-08-21 20:12:28 +0300

    adding comment

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const btn = document.getElementById("add"); // this is the button variable
 function addTask()
 // add task function, this function will let you input the tasks
 {
-    const input = document.getElementById("task-input");
+    const input = document.getElementById("task-input"); // this is input field constant
     console.log(input.value);
 }
 
