d9b8829906ff67999b11e593b3645af29fa6c9af
Osama Attia <108284913+osallines@users.noreply.github.com>
2024-08-21 20:11:21 +0300

    another coment in the js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("add"); // this is the button variable
 
-function addTask() {
+function addTask()
+// add task function, this function will let you input the tasks
+{
     const input = document.getElementById("task-input");
     console.log(input.value);
 }
