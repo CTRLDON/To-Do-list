88b77574288cf18a1b39564f96a3e6eac0e54f50
CTRLDON <a.m.elatawy@gmail.com>
2024-08-21 22:02:58 +0300

    De7k

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const btn = document.getElementById("add"); // this is the button variable
 const input = document.getElementById("task-input"); // this is input field constant
+var i = 1
 
 function addTask(){
 // add task function, this function will let you input the tasks
 // this is a new note
-
+    const x = i;
+    var text = input.value;
     const newDiv = document.createElement("div");
-    newDiv.className = `${input.value} task`;
-    newDiv.innerHTML = input.value;
+    newDiv.className = `task task${x}`;
+    newDiv.innerHTML = text;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = `task${x}`;
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.onclick = function(){
+        document.querySelector(`.task.${deleteBtn.getAttribute("class")}`).remove();
+    };
+    newDiv.appendChild(deleteBtn);
     document.body.appendChild(newDiv);
 }
 
