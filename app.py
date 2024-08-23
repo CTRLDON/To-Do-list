d4440a70b5c9baf31516b9885e6e42ba52d09b9a
CTRLDON <a.m.elatawy@gmail.com>
2024-08-23 17:32:17 +0300

    adding database

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import flask
+from flask_sqlalchemy import SQLAlchemy
 import socket
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir,"database.db")}"
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    task = db.Column(db.String(200), nullable =False)
+    status = db.Column(db.String(100) , nullable =False)
+
+    def __repr__(self):
+        return '<Task %r>' % self.task
+
 
 @app.route('/')
 def index():
     return flask.render_template("index.html")
+
+
 
 
 if __name__ == '__main__':
